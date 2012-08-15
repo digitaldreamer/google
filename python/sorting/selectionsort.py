064dcbde5dd1cc7a0c6b7c16a6e72182b2be7fee
digitaldreamer <poyzer@gmail.com>
2012-08-14 23:52:23 -0400

    selection sort

---
+++ python/sorting/selectionsort.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+def selection_sort(element):
+    """
+    O(n^2)
+
+    repeatedly identify the smallest remaining unsorted element and put it at the end of the sorted portion of the array
+    easy to program but slow
+    """
+    length = len(element)
+
+    print element
+
+    # loop through each element
+    for i in range(length):
+        placement = i
+        min_char = element[i]
+
+        # search for the minimum value
+        for j in range(i, length):
+            if element[j] < min_char:
+                min_char = element[j]
+                placement = j
+
+        # swap the pair
+        element[i], element[placement] = element[placement], element[i]
+
+        print element
+
+    return element
+
+if __name__ == '__main__':
+    sorts = selection_sort(list('SELECTIONSORT'))
